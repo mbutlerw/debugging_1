a8c4305492218715b5d1ae201a3fdda92d0448d5
Sam Morgan <s_morgan@me.com>
2016-05-25 20:31:49 +0100

    working version of the program

---
+++ spec/appointment_spec.rb
@@ -0,0 +1,15 @@
+require 'appointment'
+
+describe Appointment do
+  subject(:appointment) { described_class.new(time, "Test appointment", "50 Commercial Street London") }
+  let(:time) { Time.now }
+
+  it { is_expected.to respond_to :title }
+  it { is_expected.to respond_to :time }
+
+  describe '#pretty_location' do
+    it 'pretty-prints the location' do
+      expect(appointment.pretty_location).to eq "50 Commercial St, London E1 6LT, UK"
+    end
+  end
+end
